c57b4078c0cc32d1ace6eaeea6bc067c8b1dd542
ryan <ryan.ren@siflower.com.cn>
2018-08-23 09:48:29 +0800

    2018.08.23 test switch and a shell to control gpio.
    xxxxxxxxxxxxxxxxxx

---
+++ c/switch.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+void main ()
+{
+		int a = 2;
+		switch(a){
+				case 1: printf("case 1\n");
+						break;
+				case 2: printf("case 2\n");
+						break;
+				case 3: printf("case 3\n");
+						break;
+				case 4: printf("case 4\n");
+						break;
+}
+}
